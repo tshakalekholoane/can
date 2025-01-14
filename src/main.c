011223042915404dc7b7b58c942beef3530f810c
Tshaka Lekholoane <mail+git@tshaka.dev>
2025-01-14 13:25:59 +0200

    all: use macros

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #error "This program is intended to only run on macOS."
 #endif
 
-#if __STDC_VERSION__ < 202311L
+#if __STDC_VERSION__ < 202000
 #error "This code requires C23 or later."
 #endif
 
@@ -17,6 +17,14 @@
 #ifndef CAN_BUILD
 #define CAN_BUILD "TIP"
 #endif
+
+#define unlikely(c) __builtin_expect(!!(c), false)
+
+#define shift(n) \
+  do {           \
+    argc -= (n); \
+    argv += (n); \
+  } while (false)
 
 static const char* usage = "usage: can [-h | -V] [--] file ...";
 
@@ -78,16 +86,15 @@ int main(int argc, char* argv[argc + 1]) {
       return EXIT_FAILURE;
     }
   }
-  if (__builtin_expect(argc == 1, false)) {
+  if (unlikely(argc == 1)) {
     fprintf(stderr, "%s\n", usage);
     return EXIT_FAILURE;
   }
-  argc--;
-  argv++;
+  shift(1);
 
   for (ssize_t i = 0; i < (ssize_t)argc; i++) {
     auto name = argv[i];
-    if (__builtin_expect(strcmp(name, ".") == 0 || strcmp(name, "..") == 0 || strcmp(name, "/") == 0, false)) {
+    if (unlikely(strcmp(name, ".") == 0 || strcmp(name, "..") == 0 || strcmp(name, "/") == 0)) {
       fprintf(stderr, "\"/\", \".\", and \"..\" may not be removed.\n");
       return EXIT_FAILURE;
     }
@@ -95,19 +102,17 @@ int main(int argc, char* argv[argc + 1]) {
 
   // Avoid using the root user's trash when invoked with sudo.
   auto superuser = getenv("SUDO_USER");
-  if (__builtin_expect(superuser != nullptr, false)) {
+  if (unlikely(superuser != nullptr)) {
     auto entry = getpwnam(superuser);
-    if (__builtin_expect(!entry || seteuid(entry->pw_uid), false)) {
+    if (unlikely(!entry || seteuid(entry->pw_uid))) {
       fprintf(stderr, "%s\n", strerror(errno));
       return EXIT_FAILURE;
     }
   }
 
   // Ignore flag separator.
-  if (__builtin_expect(strcmp(argv[0], "--") == 0, false)) {
-    argc--;
-    argv++;
-  }
+  if (unlikely(strcmp(argv[0], "--") == 0))
+    shift(1);
 
   auto exit_code = EXIT_SUCCESS;
   auto file_manager = file_manager_default_manager();
@@ -115,7 +120,7 @@ int main(int argc, char* argv[argc + 1]) {
     auto path = file_manager_string_with_file_system_representation(file_manager, argv[i]);
     auto url = url_file_url_with_path(path);
     struct objc_object* err;
-    if (__builtin_expect(!file_manager_trash_item_at_url(file_manager, url, &err), false)) {
+    if (unlikely(!file_manager_trash_item_at_url(file_manager, url, &err))) {
       auto description = error_localized_description(err);
       fprintf(stderr, "%s\n", string_utf8_string(description));
       exit_code = EXIT_FAILURE;
